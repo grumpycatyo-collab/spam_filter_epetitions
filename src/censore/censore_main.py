13cbd6e5bc8be5ac79f1daac49056fd2001b96fc
GrumpyCatYo <maxpmp35@gmail.com>
2023-10-07 22:04:37 +0300

    Updated grammar_correction_main.py + minor changes in censore_main.py + add comments to funcs

--- src/censore/censore_main.py
+++ src/censore/censore_main.py
@@ -3,6 +3,17 @@ from utils.similarity import is_it_similar
 
 
 def check_censoring(sentence_str):
+    """
+    Check if a given sentence contains any bad words.
+
+    Args:
+        sentence_str (str): The sentence to be checked.
+
+    Returns:
+        bool or list:
+        - If the sentence does not contain any bad words, return False.
+        - If the sentence contains bad words, return a list of the bad words.
+    """
     words_list = tokenize_sentence(sentence_str)
     bad_words = []
     for word in words_list:
--- src/grammar_correction/grammar_correction_main.py
+++ src/grammar_correction/grammar_correction_main.py
@@ -2,6 +2,20 @@ import language_tool_python
 
 
 def print_all_necessary(seq, lang):
+    """
+    Print all necessary information for a given sequence in a specified language.
+
+    Args:
+        seq (str): The sequence to check for errors.
+        lang (str): The language of the sequence.
+
+    Returns:
+        bool or list:
+        - If there are no errors in the sequence, return False.
+        - If there are errors in the sequence, return a list of dictionaries containing the error information.
+          Each dictionary includes the offset of the error, the length of the error, and suggestions for replacement.
+
+    """
     tool = language_tool_python.LanguageTool(lang)
     matches_data = []
     errors = tool.check(seq)
